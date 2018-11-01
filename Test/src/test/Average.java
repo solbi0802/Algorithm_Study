d7e396bd3e673a66d48eaf7378e113479ab8f017
DESKTOP-J80L466\Solbi-PC <solbi5555@gmail.com>
2018-11-01 14:06:12 +0900

    BAEK JOON 10039번 문제 (배열로 다시 품)

--- Test/src/test/Average.java
+++ Test/src/test/Average.java
@@ -1,4 +1,5 @@
 package test;
+
 /* 백준 10039번 문제
 상현이가 가르치는 아이폰 앱 개발 수업의 수강생은 원섭, 세희, 상근, 숭, 강수이다.
 어제 이 수업의 기말고사가 있었고, 상현이는 지금 학생들의 기말고사 시험지를 채점하고 있다. 
@@ -12,23 +13,15 @@ import java.util.Scanner;
 public class Average {
 	public static void main(String[] args) {
 		Scanner stdIn = new Scanner(System.in);
-		int one = stdIn.nextInt(); // 원섭이 점수
-		int two = stdIn.nextInt(); // 세희 점수
-		int three = stdIn.nextInt(); // 상근이 점수
-		int four = stdIn.nextInt(); // 숭 점수
-		int five = stdIn.nextInt(); // 강수 점수
-
-		if (one < 40)
-			one = 40;
-		if (two < 40)
-			two = 40;
-		if (three < 40)
-			three = 40;
-		if (four < 40)
-			four = 40;
-		if (five < 40)
-			five = 40;
-		int avg = (one + two + three + four + five) / 5;
-		System.out.println(avg);
+		int arr[] = new int[5];
+		int avg = 0;
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = stdIn.nextInt();
+			if (arr[i] < 40)
+				arr[i] = 40;
+			avg += arr[i];
+		}
+		
+		System.out.println(avg/5);
 	}
 }
