9714387341e5112d5096243a204cfdb1e08bb8c4
DESKTOP-J80L466\Solbi-PC <solbi5555@gmail.com>
2018-11-01 13:25:52 +0900

    Chapter03 검색

---
+++ do_it/Chapter03/src/chap03/A.java
@@ -0,0 +1,20 @@
+package chap03;
+// 자연스러운 정렬을 하려면 다음과 같은 방법으로 클래스를 정의합니다.
+
+public class A implements Comparable<A> {
+	
+	// 필드 , 메서드 등
+	
+	public int compareTo(A c) {
+		//this가 c보다 크면 양의 값 반환
+		//this가 c보다 작으면 음의 값 반환
+		//this가 c와 같으면 0 반환
+		return 0;
+	}
+	
+	public boolean equals(Object c) {
+		//this가 c와 같으면 true 반환
+		//this가 c와 같지 않으면 false 반환
+		return false;
+	}
+}
---
+++ do_it/Chapter03/src/quiz/Quiz6.java
@@ -0,0 +1,38 @@
+package quiz;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// 검색 실패시 삽입 포인트 값을 출력
+public class Quiz6 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.print("요솟수 :");
+		int num = stdIn.nextInt();
+		int[] x = new int[num];
+
+		System.out.println("오름차순으로 입력하세요.");
+
+		System.out.print("x[0] :"); // 배열의 첫 요소를 먼저 입력합니다.
+		x[0] = stdIn.nextInt();
+
+		for (int i = 1; i < num; i++) {
+			do {
+				System.out.print("x[" + i + "] :");
+				x[i] = stdIn.nextInt();
+			} while (x[i] < x[i - 1]); // 바로 앞의 요소보다 작으면 다시 입력합니다.
+		}
+
+		System.out.print("검색할 값 :");
+		int ky = stdIn.nextInt();
+
+		int idx = Arrays.binarySearch(x, ky);
+
+		if (idx < 0) {
+			System.out.println("그 값의 요소가 없습니다.");
+			System.out.println("삽입 포인트는 " + (-idx - 1) + "입니다.");
+		} else
+			System.out.println(ky + "은(는) x[" + idx + "]에 있습니다.");
+	}
+}
