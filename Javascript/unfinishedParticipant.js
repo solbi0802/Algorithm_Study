0e3ee28dd0d1c101e5116391dfa661166002cfa3
unknown <solbi5555@gmail.com>
2021-03-24 22:29:09 +0900

    의미있는 함수명으로 변경

--- Javascript/unfinishedParticipant.js
+++ Javascript/unfinishedParticipant.js
@@ -28,7 +28,7 @@ participant	                                        completion	                 
 "mislav"는 참여자 명단에는 두 명이 있지만, 완주자 명단에는 한 명밖에 없기 때문에 한명은 완주하지 못했습니다.
 */
 
-function solution(participant, completion) {
+function searchUnfinishedParticipant(participant, completion) {
     // 배열 정렬
     participant.sort()
     completion.sort()
