efb8f1bbeae603bf93d4ea45afa56d012dd7cfa5
solbi9382 <solbi5555@gmail.com>
2021-03-30 22:58:55 +0900

    잘못 복사된 코드 수정

--- Javascript/star.js
+++ Javascript/star.js
@@ -23,18 +23,15 @@ n과 m은 각각 1000 이하인 자연수입니다.
 
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
-    process.stdin.setEncoding('utf8');
-    process.stdin.on('data', data => {
-        const n = data.split(" ");
-        const a = Number(n[0]), b = Number(n[1]);
-        let star = ''
-        
-        for (let i = 0; i < b; i++) {
-            for (let j = 0; j < a; j++) {
-                star += '*'
-            }
-            star += '\n'
+    const n = data.split(" ");
+    const a = Number(n[0]), b = Number(n[1]);
+    let star = ''
+    
+    for (let i = 0; i < b; i++) {
+        for (let j = 0; j < a; j++) {
+            star += '*'
         }
-        console.log(star)
-    })
+        star += '\n'
+    }
+    console.log(star)
 });
