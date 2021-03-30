55fa8466f33ae9f94219e5e4ff36c640343f6e38
solbi9382 <solbi5555@gmail.com>
2021-03-30 22:41:26 +0900

    아스키코드로 푸는 방법 추가

--- Javascript/onlyNumber.js
+++ Javascript/onlyNumber.js
@@ -12,18 +12,31 @@ s는 길이 1 이상, 길이 8 이하인 문자열입니다.
 */
 
 // 정규 표현식 사용 
-const onlyNumber = (s => {
+const onlyNumber = s => {
     // s의 길이와 숫자외에 다른 문자가 있는 지 확인
     return  /^([0-9]{4}|[0-9]{6})$/.test(s)
-})
+}
 
 // 입력 받은 문자열을 자른 후 배열에 저장 후 문자열마다 타입 확인
 // 테스트 실패하는 케이스 존재(87.5점)
-const isNaN = (str => {    
+const isNaN = str => {    
     for (const letter of str) {
       if (isNaN(parseInt(letter))) {
           return false
       }
     }
       return true
-})
+}
+
+// 아스키 코드로 문자,숫자 판별 (100점)
+const asciiDiff = str => {    
+    if (str.length === 4 || str.length === 6) { 
+        for (const letter of str) { 
+            if (letter < "0" || letter > "9") { 
+                return false 
+            } 
+        } return true 
+    }else { 
+        return false 
+    } 
+}
