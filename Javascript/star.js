42f21cb80f66bec52b3bf6a25638ffe143404483
solbi9382 <solbi5555@gmail.com>
2021-03-31 22:25:18 +0900

    의미있게 변수명 수정

--- Javascript/star.js
+++ Javascript/star.js
@@ -23,12 +23,12 @@ n과 m은 각각 1000 이하인 자연수입니다.
 
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
-    const n = data.split(" ");
-    const a = Number(n[0]), b = Number(n[1]);
+    const str = data.split(" ");
+    const x = Number(str[0]), y = Number(str[1]);
     let star = ''
     
-    for (let i = 0; i < b; i++) {
-        for (let j = 0; j < a; j++) {
+    for (let i = 0; i < y; i++) {
+        for (let j = 0; j < x; j++) {
             star += '*'
         }
         star += '\n'
